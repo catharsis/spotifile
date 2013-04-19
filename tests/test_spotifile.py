3059e2cf76e2e7bfb90c6c03afc5ee372294de94
Anton Lofgren <alofgren@op5.com>
2013-04-19 15:41:44 +0200

    tests: use with_setup instead of setUp/tearDown
    It makes the tests more readable, and allows us to write less
    boilerplate to add new tests with different fixtures (logged
    out/connectionless/etc.)
    
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

--- tests/test_spotifile.py
+++ tests/test_spotifile.py
@@ -1,26 +1,27 @@
-import unittest
+from nose import with_setup
 import os
+from os import path
 from subprocess import check_call
-from sh import ls
+from sh import ls, cat
 
 
 mountpoint = '/tmp/spotifile_test_mount'
-class SpotifileTestClass(unittest.TestCase):
-	@classmethod
-	def setUpClass(cls):
-		if not os.path.exists(mountpoint):
-			os.mkdir(mountpoint)
 
-	@classmethod
-	def tearDownClass(cls):
-		if os.path.exists(mountpoint):
-			os.rmdir(mountpoint)
+def fs_mount():
+	if not path.exists(mountpoint):
+		os.mkdir(mountpoint)
+	check_call(['./spotifile', mountpoint])
 
-	def setUp(self):
-		check_call(['./spotifile', mountpoint])
+def fs_unmount():
+	check_call(['fusermount', '-u', mountpoint])
+	if path.exists(mountpoint):
+		os.rmdir(mountpoint)
 
-	def tearDown(self):
-		check_call(['fusermount', '-u', mountpoint])
+@with_setup(fs_mount, fs_unmount)
+def test_ls():
+	assert 'connection' in ls(mountpoint)
 
-	def test_ls(self):
-		assert 'connection' in ls(mountpoint)
+@with_setup(fs_mount, fs_unmount)
+def test_cat_connection():
+	assert 'logged in' in cat(path.join(mountpoint, 'connection'))
+
