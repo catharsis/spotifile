25dea125e8acf853650afb98e374e1b2dbe4429d
Anton Lofgren <alofgren@op5.com>
2013-04-25 11:30:27 +0200

    Use callback for connection file getattr

--- spfs_fuse.c
+++ spfs_fuse.c
@@ -2,23 +2,53 @@
 #include <string.h>
 #include <errno.h>
 extern time_t g_logged_in_at;
+
+size_t connection_file_read(char *buf, size_t size, off_t offset);
+void connection_file_getattr(struct stat *statbuf);
+
+struct spfs_file {
+	char *abs_path;
+	void (*spfs_file_getattr)(struct stat *);
+	size_t (*spfs_file_read)(char *, size_t, off_t);
+};
+
+static struct spfs_file spfs_files[] = {
+	{ /*connection file*/
+		.abs_path = "/connection",
+		.spfs_file_getattr = connection_file_getattr,
+		.spfs_file_read = connection_file_read
+	},
+	/*SENTINEL*/
+	{NULL},
+};
+
 int spfs_getattr(const char *path, struct stat *statbuf)
 {
-	int res = 0;
+	int i = 0;
+	struct spfs_file *tmpfile = NULL;
 	memset(statbuf, 0, sizeof(struct stat));
 	if(strcmp(path, "/") == 0) {
 		statbuf->st_mode = S_IFDIR | 0755;
 		statbuf->st_nlink = 2;
+		return 0;
 	}
-	else if (strcmp(path, "/connection") == 0) {
-		statbuf->st_mode = S_IFREG | 0444;
-		statbuf->st_nlink = 1;
-		statbuf->st_size = 64;
-		statbuf->st_mtime = g_logged_in_at;
+	else {
+		while ((tmpfile = &spfs_files[i++])) {
+			if (strcmp(tmpfile->abs_path, path) == 0) {
+				tmpfile->spfs_file_getattr(statbuf);
+				return 0;
+			}
+		}
 	}
-	else
-		res = -ENOENT;
-	return res;
+	return -ENOENT;
+}
+
+void connection_file_getattr(struct stat *statbuf)
+{
+	statbuf->st_mode = S_IFREG | 0444;
+	statbuf->st_nlink = 1;
+	statbuf->st_size = 64;
+	statbuf->st_mtime = g_logged_in_at;
 }
 
 size_t connection_file_read(char *buf, size_t size, off_t offset) {
@@ -40,8 +70,13 @@ size_t connection_file_read(char *buf, size_t size, off_t offset) {
 int spfs_read(const char *path, char *buf, size_t size, off_t offset,
 		struct fuse_file_info *fi)
 {
-	if (!strcmp(path, "/connection"))
-		return connection_file_read(buf, size, offset);
+	int i = 0;
+	struct spfs_file *tmpfile;
+	while((tmpfile = &spfs_files[i++]) != NULL)
+	{
+		if (strcmp(tmpfile->abs_path, path) == 0)
+			return tmpfile->spfs_file_read(buf, size, offset);
+	}
 	return -ENOENT;
 
 }
