c2bba31dab75acbccee542f94e01f7feb16e6cd4
Anton Lofgren <anton.lofgren@gmail.com>
2015-08-05 14:09:39 +0200

    janitor: Return early when no albums available
    This makes the code a bit easier to follow.
    
    Signed-off-by: Anton Lofgren <anton.lofgren@gmail.com>

--- src/spfs_fuse_artist.c
+++ src/spfs_fuse_artist.c
@@ -16,29 +16,30 @@ static int albums_readdir(const char *path, void *buf, fuse_fill_dir_t filler, o
 		struct fuse_file_info *fi) {
 	spfs_entity *e = (spfs_entity *)fi->fh;
 	GArray *albums = spotify_get_artistbrowse_albums(e->parent->auxdata);
-	if (albums) {
-		for (guint i = 0; i < albums->len; ++i) {
-			sp_album *album = g_array_index(albums, sp_album *, i);
+	if (!albums)
+		return 0;
 
-			spfs_entity *album_browse_dir = create_album_browse_dir(album);
-			const gchar *album_name = spotify_album_name(album);
-			/* FIXME: Deal with duplicates (re-releases, region availability.) properly
-			 * Probably by presenting the album that has the most tracks available
-			 * to the user as suggested here: http://stackoverflow.com/a/11994581
-			 *
-			 * XXX: Perhaps, it'd be a good idea to also add the release year to the album title
-			 * like "Ride The Lightning (1984)" to further differentiate between re-releases
-			 */
-			if (!spfs_entity_dir_has_child(e->e.dir, album_name)) {
-				spfs_entity *album_link = spfs_entity_link_create(album_name, NULL);
-				spfs_entity_dir_add_child(e, album_link);
-				gchar *rpath = relpath(e, album_browse_dir);
-				spfs_entity_link_set_target(album_link, rpath);
-				g_free(rpath);
-			}
+	for (guint i = 0; i < albums->len; ++i) {
+		sp_album *album = g_array_index(albums, sp_album *, i);
+
+		spfs_entity *album_browse_dir = create_album_browse_dir(album);
+		const gchar *album_name = spotify_album_name(album);
+		/* FIXME: Deal with duplicates (re-releases, region availability.) properly
+		 * Probably by presenting the album that has the most tracks available
+		 * to the user as suggested here: http://stackoverflow.com/a/11994581
+		 *
+		 * XXX: Perhaps, it'd be a good idea to also add the release year to the album title
+		 * like "Ride The Lightning (1984)" to further differentiate between re-releases
+		 */
+		if (!spfs_entity_dir_has_child(e->e.dir, album_name)) {
+			spfs_entity *album_link = spfs_entity_link_create(album_name, NULL);
+			spfs_entity_dir_add_child(e, album_link);
+			gchar *rpath = relpath(e, album_browse_dir);
+			spfs_entity_link_set_target(album_link, rpath);
+			g_free(rpath);
 		}
-		g_array_free(albums, false);
 	}
+	g_array_free(albums, false);
 	return 0;
 }
 
