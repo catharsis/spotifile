92567956c1a22064210ae4922d490e31df365856
Anton Lofgren <anton.lofgren@gmail.com>
2015-09-02 08:30:48 +0200

    An attempt at a sane playlist directory structure
    Subdiring each playlist directly (with a pure music dir) turned out to
    be suboptimal. This moves the music dir distinction one level up, in
    order to explore a solution where we'll be able to point at *one*
    directory and say "This is where the music is".
    
    This is nice, because it allows us to do
    
    ~/spotifile/playlists/music$ mplayer -shuffle */*.wav
    
    without loading a lot of superfluous metadata.
    
    Signed-off-by: Anton Lofgren <anton.lofgren@gmail.com>

--- src/spfs_fuse_playlist.h
+++ src/spfs_fuse_playlist.h
@@ -2,5 +2,8 @@
 #define SPFS_FUSE_PLAYLIST_H
 #include "spfs_fuse.h"
 #include "spfs_fuse_entity.h"
-int playlists_dir_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi);
+int playlists_music_dir_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
+		struct fuse_file_info *fi);
+int playlists_meta_dir_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
+		struct fuse_file_info *fi);
 #endif /* SPFS_FUSE_PLAYLIST_H */
