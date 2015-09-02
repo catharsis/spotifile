5481b86e8e975387329591534a2956ca1a576481
Anton Lofgren <anton.lofgren@gmail.com>
2015-09-02 08:30:48 +0200

    An attempt at a sane playlist directory structure
    Subdiring each playlist directly (with a pure music dir) turned out to
    be suboptimal. This moves the music dir distinction one level up, in
    order to explore a solution where we'll be able to point at *one*
    directory and say "This is where the music is".
    
    This is nice, because it allows us to do
    
    ~/spotifile/playlists/music$ mplayer -shuffle */*.wav
    
    without loading a lot of superfluous metadata.
    
    Signed-off-by: Anton Lofgren <anton.lofgren@gmail.com>

--- src/spfs_fuse_playlist.c
+++ src/spfs_fuse_playlist.c
@@ -3,7 +3,7 @@
 #include "spfs_fuse_playlist.h"
 #include "spfs_fuse_track.h"
 /*readdir for a single playlist directory*/
-static int playlist_dir_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
+static int playlist_dir_meta_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
 		struct fuse_file_info *fi) {
 
 	spfs_entity *e = SPFS_FH2ENT(fi->fh);
@@ -30,8 +30,32 @@ static int playlist_dir_readdir(const char *path, void *buf, fuse_fill_dir_t fil
 	return 0;
 }
 
-/*readdir for the "root" playlists directory*/
-int playlists_dir_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
+static int playlist_dir_music_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
+		struct fuse_file_info *fi) {
+
+	spfs_entity *e = SPFS_FH2ENT(fi->fh);
+	sp_playlist *pl = (sp_playlist *)e->auxdata;
+	GArray *tracks = spotify_get_playlist_tracks(pl);
+	for (guint i = 0; i < tracks->len; ++i) {
+		sp_track *track = g_array_index(tracks, sp_track *, i);
+		gchar *trackname = spotify_track_name(track);
+		gchar *formatted_trackname = g_strdup_printf("%.2d - %s.wav", i, trackname);
+		g_free(trackname);
+
+		time_t track_create_time = spotify_playlist_track_create_time(pl, i);
+		if (!spfs_entity_dir_has_child(e->e.dir, formatted_trackname)) {
+			spfs_entity *wav_file= create_track_wav_file(formatted_trackname, track);
+			wav_file->mtime = track_create_time;
+			spfs_entity_dir_add_child(e, wav_file);
+		}
+		g_free(formatted_trackname);
+	}
+	g_array_free(tracks, false);
+	return 0;
+}
+
+/*readdir for the "music" playlists directory*/
+int playlists_music_dir_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
 		struct fuse_file_info *fi) {
 	sp_session *session = SPFS_SP_SESSION;
 	g_return_val_if_fail(session != NULL, 0);
@@ -42,7 +66,31 @@ int playlists_dir_readdir(const char *path, void *buf, fuse_fill_dir_t filler, o
 		gchar *name = spotify_playlist_name(pl);
 		if (!spfs_entity_dir_has_child(playlists_dir->e.dir, name)) {
 			spfs_entity * pld = spfs_entity_dir_create(name,
-					&(struct spfs_dir_ops) {.readdir = playlist_dir_readdir});
+					&(struct spfs_dir_ops) {.readdir = playlist_dir_music_readdir});
+			pld->auxdata = pl;
+			spfs_entity_dir_add_child(playlists_dir, pld);
+		}
+
+		g_free(name);
+	}
+	g_array_free(playlists, false);
+
+	return 0;
+}
+
+/*readdir for the "meta" playlists directory*/
+int playlists_meta_dir_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
+		struct fuse_file_info *fi) {
+	sp_session *session = SPFS_SP_SESSION;
+	g_return_val_if_fail(session != NULL, 0);
+	spfs_entity *playlists_dir = SPFS_FH2ENT(fi->fh);
+	GArray *playlists = spotify_get_playlists(session);
+	for (guint i = 0; i < playlists->len; ++i) {
+		sp_playlist * pl = g_array_index(playlists, sp_playlist *, i);
+		gchar *name = spotify_playlist_name(pl);
+		if (!spfs_entity_dir_has_child(playlists_dir->e.dir, name)) {
+			spfs_entity * pld = spfs_entity_dir_create(name,
+					&(struct spfs_dir_ops) {.readdir = playlist_dir_meta_readdir});
 			pld->auxdata = pl;
 			spfs_entity_dir_add_child(playlists_dir, pld);
 		}
--- src/spfs_fuse_track.h
+++ src/spfs_fuse_track.h
@@ -3,5 +3,6 @@
 #include <libspotify/api.h>
 #include "spfs_fuse.h"
 #include "spfs_fuse_entity.h"
+spfs_entity *create_track_wav_file(const gchar *name, sp_track *track);
 spfs_entity *create_track_browse_dir(sp_track *track);
 #endif /* SPFS_FUSE_TRACK_H */
