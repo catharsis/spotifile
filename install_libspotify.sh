fd7fb038509004515e81921315712e5e2a17b1cc
Anton Löfgren <anton.lofgren@gmail.com>
2016-01-04 16:04:08 +0100

    syntax better
    Signed-off-by: Anton Löfgren <anton.lofgren@gmail.com>

--- install_libspotify.sh
+++ install_libspotify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-SPOTIFILE_MARCH=$(SPOTIFILE_MARCH:-x86_64)
+SPOTIFILE_MARCH=${SPOTIFILE_MARCH:-x86_64}
 PKG_NAME="libspotify-12.1.51-Linux-$(SPOTIFILE_MARCH)-release"
 wget https://developer.spotify.com/download/libspotify/$(PKG_NAME).tar.gz
 tar -xvf $(PKG_NAME).tar.gz
