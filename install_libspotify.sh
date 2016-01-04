b475565abd90082df643d07e9c8ffe2aaafc9b11
Anton Löfgren <anton.lofgren@gmail.com>
2016-01-04 16:06:08 +0100

    syntax better.. revisited
    Signed-off-by: Anton Löfgren <anton.lofgren@gmail.com>

--- install_libspotify.sh
+++ install_libspotify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 SPOTIFILE_MARCH=${SPOTIFILE_MARCH:-x86_64}
-PKG_NAME="libspotify-12.1.51-Linux-$(SPOTIFILE_MARCH)-release"
+PKG_NAME="libspotify-12.1.51-Linux-${SPOTIFILE_MARCH}-release"
 wget https://developer.spotify.com/download/libspotify/$(PKG_NAME).tar.gz
 tar -xvf $(PKG_NAME).tar.gz
 cd $(PKG_NAME) && patch < ../libspotify-Makefile.patch && make prefix=$HOME/libspotify install
