6839a1d6fbccd7f7cf26af9c8a52f6addba6eb94
Gianguido Sorà <g.sora4@gmail.com>
2016-03-11 11:59:49 +0100

    escape amperstand in playlist creation
    right now, this library only contains a function to escape all the
    amperstands contained in a given string, but if needed, will contain all
    the sanitize functions for the xspf format

---
+++ src/xspf_sanitize.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "xspf_sanitize.h"
+#include "string_utils.h"
+#include <stdio.h>
+
+const gchar * replace_all(const gchar * str, const gchar *key, const gchar * val) {
+    gchar * tmpstr = str_replace(str, key, val);
+    while (strstr(tmpstr, key) != NULL) {
+        gchar *old = tmpstr;
+        tmpstr = str_replace(tmpstr, key, val);
+        g_free(old);
+    }
+
+    return tmpstr;
+}
+
+gchar * xspf_escape_amperstand(const gchar * s) {
+    gchar * tmpstr = str_replace(s, "&", "!amp;");
+    replace_all(tmpstr, "!amp;", "&amp;");
+    return tmpstr;
+}
---
+++ src/xspf_sanitize.h
@@ -0,0 +1,5 @@
+#ifndef XSPF_SANITIZE_H
+#define XSPF_SANITIZE_H
+#include <glib.h>
+gchar * xspf_escape_amperstand(const gchar * s);
+#endif /* XSPF_SANITIZE_H */
